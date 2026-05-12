6231074b8b8f8b4fd1db39cf0adb62e62b9a6797
phamvanvuhoan <vuhoan2262005@gmail.com>
2026-05-12 14:35:48 +0700

    update

--- src/gector/__init__.py
+++ src/gector/__init__.py
@@ -1,6 +1,6 @@
 from .modeling import GECToR
 from .configuration import GECToRConfig
-from .dataset import load_dataset, GECToRDataset
+from .dataset import load_dataset, preprocess_dataset, GECToRDataset
 from .predict import predict, load_verb_dict
 from .predict_verbose import predict_verbose
 from .vocab import (
